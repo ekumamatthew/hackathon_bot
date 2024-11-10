5201646394a2805993a30d55e1460da9154f3c6a
iamnovichek <rungotika@gmail.com>
2024-11-10 21:04:02 +0100

    docstrings added

--- tracker/admin.py
+++ tracker/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import Group
 from django.db.models import QuerySet
 from django.forms import BaseModelForm
 from django.utils.html import format_html
+from django.utils.safestring import SafeString
 
 from .models import Repository
 from .telegram.bot import create_tg_link
@@ -14,9 +15,30 @@ admin.site.unregister(Group)
 
 @admin.register(Repository)
 class RepositoryAdmin(admin.ModelAdmin):
+    """
+    Custom Django admin class to manage Repository objects.
+
+    This class customizes the Django admin interface for Repository models,
+    providing specific configurations like setting default user properties
+    and generating a referral link for each repository.
+
+    Methods:
+        telegram_link: Adds a referral link to display in the list view.
+        get_form: Customizes the model form to set the user field to the current user.
+        get_queryset: Filters the queryset to show only the current user's repositories.
+
+    Attributes:
+        list_display (tuple): Fields to be displayed in the list view of the admin panel.
+    """
     list_display = ("name", "author", "telegram_link")
 
-    def telegram_link(self, obj):
+    def telegram_link(self, obj) -> SafeString:
+        """
+        A function that adds referal link to object's display list
+        :param obj:
+        :return: SafeString
+        """
+
         link = asyncio.run(create_tg_link(obj.user.id))
 
         return format_html(
