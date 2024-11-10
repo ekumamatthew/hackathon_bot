4e8022690d71c5c65fabc09a395485e6fd511c92
iamnovichek <rungotika@gmail.com>
2024-11-10 21:06:25 +0100

    build flow updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,7 @@ python manage.py collectstatic --no-input
 echo "Applying migrations"
 python manage.py migrate
 
-echo "Creating superuser"
-python manage.py create_superuser
-
 echo "Starting the server and bot..."
 exec "$@"
 
-gunicorn --bind 0.0.0.0:8000 core.wsgi:application & python -m tracker.telegram.bot
+gunicorn --bind 0.0.0.0:8000 core.wsgi:application & python manage.py run_telegram_bot
