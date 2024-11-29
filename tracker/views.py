c6eb6ae6f02cd92cb1d792c6ba03d2b43bb59a03
aurlic <arthur.urlic@gmail.com>
2024-11-29 18:40:15 +0100

    feat: view for contributors

--- tracker/views.py
+++ tracker/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth import login
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import CreateView
+from django.views import View
+from django.http import JsonResponse
 
 from .forms import SignUpForm
-
+from .models import Contributor
 
 class CreateUserView(CreateView):
     form_class = SignUpForm
@@ -43,3 +45,36 @@ class CreateUserView(CreateView):
         [messages.error(request, error_) for error_ in form.errors.values()]
 
         return render(request, self.template_name, {"form": form})
+
+
+class ContributorListView(View):
+    """
+    View to list contributors. Only project leads can view notes and rank
+    """
+    
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        contributors = Contributor.objects.all()
+        
+        if user.is_project_lead():
+            data = [
+                {
+                    "id": contributor.id,
+                    "user": contributor.user.email,
+                    "role": contributor.role,
+                    "notes": contributor.notes,
+                    "rank": contributor.rank,
+                }
+                for contributor in contributors
+            ]
+        else:
+            data = [
+                {
+                    "id": contributor.id,
+                    "user": contributor.user.email,
+                    "role": contributor.role,
+                }
+                for contributor in contributors
+            ]
+        
+        return JsonResponse(data, safe=False)
