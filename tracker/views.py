ea8a970dbdf23b0c8fabf29e52fdd189d3ff6fe6
aurlic <arthur.urlic@gmail.com>
2024-11-30 10:48:26 +0100

    refactor: moved ContributorView logic to admin.py

--- tracker/views.py
+++ tracker/views.py
@@ -4,10 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import CreateView
 from django.views.generic.list import ListView
-from django.http import JsonResponse
 
 from .forms import SignUpForm
-from .models import Contributor
 
 class CreateUserView(CreateView):
     form_class = SignUpForm
@@ -45,52 +43,3 @@ class CreateUserView(CreateView):
         [messages.error(request, error_) for error_ in form.errors.values()]
 
         return render(request, self.template_name, {"form": form})
-
-
-class ContributorListView(ListView):
-    """
-    A view to list contributors with role-based data visibility.
-    """
-
-    model = Contributor
-
-    def get_queryset(self):
-        """
-        Customize the queryset based on the user's role.
-        Project leads can see notes and rank, while others cannot.
-        """
-        user = self.request.user
-
-        if user.is_project_lead():
-            return Contributor.objects.all()
-        return Contributor.objects.all().only("id", "user", "role")
-
-    def render_to_response(self, context, **response_kwargs):
-        """
-        Customize the JSON response to include contributor data.
-        """
-        queryset = self.get_queryset()
-        user = self.request.user
-
-        if user.is_project_lead():
-            data = [
-                {
-                    "id": contributor.id,
-                    "user": contributor.user.telegramuser.telegram_id,
-                    "role": contributor.role,
-                    "notes": contributor.notes,
-                    "rank": contributor.rank,
-                }
-                for contributor in queryset
-            ]
-        else:
-            data = [
-                {
-                    "id": contributor.id,
-                    "user": contributor.user.telegramuser.telegram_id,
-                    "role": contributor.role,
-                }
-                for contributor in queryset
-            ]
-
-        return JsonResponse(data, safe=False, **response_kwargs)
